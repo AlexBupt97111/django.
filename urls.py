3e52e47f8c9011ca2517203a713fea63b7517a40
AlexBupt97111 <95982030+AlexBupt97111@users.noreply.github.com>
2022-10-31 16:07:24 +0200

    Update urls.py

--- urls.py
+++ urls.py
@@ -17,7 +17,7 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 
-from trainsite import settings # settings - module
+from trainsite import settings #settings - module
 from women.views import *
 from django.urls import path, include
 
